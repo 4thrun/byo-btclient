f92c99f1df2150fe6a9decbe970b0414c8ae9026
4thrun <65476356+4thrun@users.noreply.github.com>
2024-08-25 17:58:49 +0800

    test handshake

---
+++ handshake/handshake_test.go
@@ -0,0 +1,96 @@
+package handshake
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHandshake_Serialize(t *testing.T) {
+	var tests = map[string]struct {
+		input  *Handshake
+		output []byte
+	}{
+		"standard <pstr>": {
+			input: &Handshake{
+				PstrLength: byte(19),
+				Pstr:       "BitTorrent protocol",
+				InfoHash:   [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116},
+				PeerID:     [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			},
+			output: []byte{19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+		},
+		"different <pstr>": {
+			input: &Handshake{
+				PstrLength: byte(32),
+				Pstr:       "BitTorrent protocol, but cooler?",
+				InfoHash:   [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116},
+				PeerID:     [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			},
+			output: []byte{32, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 44, 32, 98, 117, 116, 32, 99, 111, 111, 108, 101, 114, 63, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+		},
+	}
+	for _, test := range tests {
+		var buf, _ = test.input.Serialize()
+		assert.Equal(t, buf, test.output)
+	}
+}
+
+func TestNew(t *testing.T) {
+	infoHash := [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116}
+	peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	reserved := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
+	h, _ := New(infoHash, peerID, reserved)
+	expected := &Handshake{
+		Pstr:       "BitTorrent protocol",
+		InfoHash:   [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116},
+		PeerID:     [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+		PstrLength: byte(19),
+	}
+	assert.Equal(t, expected, h)
+}
+
+func TestRead(t *testing.T) {
+	tests := map[string]struct {
+		input   []byte
+		output  *Handshake
+		failure bool
+	}{
+		"parse handshake into struct": {
+			input: []byte{19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+			output: &Handshake{
+				Pstr:       "BitTorrent protocol",
+				InfoHash:   [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116},
+				PeerID:     [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+				PstrLength: byte(19),
+			},
+			failure: false,
+		},
+		"empty": {
+			input:   []byte{},
+			output:  nil,
+			failure: true,
+		},
+		"not enough bytes": {
+			input:   []byte{19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111},
+			output:  nil,
+			failure: true,
+		},
+		"<pstrlen> is 0": {
+			input:   []byte{0, 0, 0},
+			output:  nil,
+			failure: true,
+		},
+	}
+
+	for _, test := range tests {
+		reader := bytes.NewReader(test.input)
+		m, err := Read(reader)
+		if test.failure {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+		assert.Equal(t, test.output, m)
+	}
+}
